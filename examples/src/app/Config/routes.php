fb0e90dfc0b324419a072c87550b5992847f1851
FaircoreHD <nafis23harwin2001@gmail.com>
2024-03-09 12:40:50 +0700

    testing route

--- examples/src/app/Config/routes.php
+++ examples/src/app/Config/routes.php
@@ -17,6 +17,17 @@ class MainController {
         return new Response('Hello there, it works !');
     }
 
+    function welcome() {
+        return new Response('Routes work');
+    }
+
+    function getRuang() {
+        $db = Manager::get('main');
+        $ruang = $db->order(['nama' => 'ASC'])->limit(10)->get('ruang');
+        return new Response(json_encode(['data' => $ruang->result()], JSON_PRETTY_PRINT));
+    }
+
 }
 
+Route::get('/ruang', 'MainController@getRuang');
 Route::get('/{idPoliklinik}', 'MainController@index');
