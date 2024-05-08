2a877058fd316d832b700d8734fbf66a0d17aa67
FaircoreHD <nafis23harwin2001@gmail.com>
2024-05-08 09:43:01 +0700

    add helper for buildSearch and buildOrder (not tested)

--- src/Helpers.php
+++ src/Helpers.php
@@ -50,3 +50,24 @@ if(!function_exists('redirect')) {
         exit();
     }
 }
+
+if(!function_exists('buildSearch')) {
+    function buildSearch($cols, $str) {
+        $orWhere = [];
+        foreach($cols as $col) {
+            $orWhere[] = [$col, 'LIKE', '%'.$str.'%'];
+        }
+        return $orWhere;
+    }
+}
+
+if(!function_exists('buildOrder')) {
+    function buildOrder($str) {
+        $order = [];
+        foreach(explode(',', $str) as $s) {
+            $a = explode(':', $s);
+            $order[$a[0]] = $a[1];
+        }
+        return $order;
+    }
+}
