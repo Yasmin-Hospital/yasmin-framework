6e9ee2ec749a85652b2179225ae8abc7c1e72c7b
mochrira <mochrira@gmail.com>
2024-05-10 20:06:49 +0700

    remove unnecessary examples

--- examples/src/index.php
+++ examples/src/index.php
@@ -1,14 +1,88 @@
 <?php 
 
+declare(strict_types=1);
+
 require 'vendor/autoload.php';
 
-define('BASEPATH', __DIR__);
+use Yasmin\Response;
+use Yasmin\Route;
+use Yasmin\Framework;
+use Yasmin\Uri;
+use Yasmin\Database\Manager;
 
-date_default_timezone_set('Asia/Jakarta');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, authorization");
-header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PATCH, DELETE");
+/** Simplest way */
+Route::get('/',function() { 
+    return new Response('Index');
+});
 
-require BASEPATH.'/app/Config/routes.php';
-require BASEPATH.'/app/Config/database.php';
-Yasmin\Framework::run();
+/** Function Name */
+function functionName() {
+    return new Response('Function Name');
+}
+Route::get('/function','functionName');
+
+/** Class and Method */
+Route::get('/home', '\App\Controllers\HomeController@index');
+
+/** with URI Parameters, you can implement it to another style */
+Route::get('/function/{name}', function (string $name) {
+    return new Response(json_encode([
+        'name' => $name
+    ], JSON_PRETTY_PRINT));
+});
+
+/** with Dependency Injection, you can implement it to another style */
+Route::get('/dependency/{name}', function (string $name, Uri $uri) {
+    return new Response(json_encode([
+        'name' => $name,
+        'baseUrl' => $uri->baseUrl(),
+        'currentUrl' => $uri->currentUrl(),
+        'currentUri' => $uri->string(),
+        'segments' => $uri->segments()
+    ], JSON_PRETTY_PRINT));
+});
+
+$config = json_decode(file_get_contents(__DIR__.'/private/.DBCONFIG'), true);
+$configMysql = [
+    "driver" => "mysql",
+    "host"=>"mariadb.database",
+    "username" => "root",
+    "password" => "root"
+];
+Manager::add('slave', $configMysql);
+Manager::add('main', $config);
+
+Route::get('/db', function () {
+    $db = Manager::get('main');
+    $mysqlDb = Manager::get('slave');
+    
+    $db->connect();
+
+    $mysqlDb->connect();
+    $mysqlDb->select_db("test");
+
+    // sql server
+    $penggunaSqlServer = $db->limit(4)->get("Pengguna");
+
+
+    // mysql
+    $PenggunaMysql = $mysqlDb->order("Pengguna.PenggunaId")->limit(2)->offset(3)->get('Pengguna');
+
+    return new Response(json_encode([
+        // 'connect' => $connectRes,
+        // 'disconnect' => $disconnectRes
+        // 'kontak' => [
+        //     'num_rows' => $queryKontak->num_rows(),
+        //     'result' => $queryKontak->result(),
+        //     'row' => $queryKontak->row()
+        // ]
+        'penggunaSQLServer'=> [
+            'result'=>$penggunaSqlServer->result()
+        ],
+        'penggunaMYSQL'=> [
+            'result'=>$PenggunaMysql->result()
+        ]
+    ], JSON_PRETTY_PRINT));
+});
+
+Framework::run();
